fb00ae41ba99125a48ac9225c74876f5da187180
jangjinseong <hn04223@gmail.com>
2024-11-19 16:40:17 +0900

    2024 11 19

---
+++ src/main/java/com/green/greengramver1/common/SwaggerConfiguration.java
@@ -0,0 +1,14 @@
+package com.green.greengramver1.common;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "GreenGram",
+                description = "그린그램 SNS",
+                version = "v1"
+        )
+)
+public class SwaggerConfiguration {
+}
